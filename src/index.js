f0e21a330c1cbdc143d7c550a491950758d25b05
vladimir <lovkiy2012@gmail.com>
2021-05-29 22:46:22 +0300

    create phones reducer

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 import './main.css'
-
+import {createStore} from 'redux'
+import {createBrowserHistory} from 'history'
+const history = createBrowserHistory()
+const store = createStore(createRootReducer(history))
 document.write('Hello React/Redux!')
